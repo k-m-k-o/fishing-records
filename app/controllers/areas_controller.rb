f23a746155173eeeb7f20d2ce60cd1a069e0f7aa
kazuki mokuo <sp9t5ns9@icloud.com>
2019-08-24 18:40:15 +0900

    ルーティング、コントローラー、ビューの追加

---
+++ app/controllers/areas_controller.rb
@@ -0,0 +1,26 @@
+class AreasController < ApplicationController
+
+  def index
+  end  
+
+  def show
+  end
+
+  def new
+  end
+  
+  def create
+  end
+
+  def edit
+  end
+  
+  def update
+  end
+  
+  private
+
+  def area_params
+  end  
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only: [:show]
+  resources :posts
+  resources :fish ,only: [:index]
+  resources :prefectures, only: [:index] do
+    resources :cities ,only: [:index] do
+      resources :areas ,except: [:destroy]
+    end
+  end  
+  resources :search ,only: [:index]
 end
